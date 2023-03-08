99437737e6eb9dd4bfc9f7a52610bc30312f27e2
Saif Almaliki <saif.wsm@gmail.com>
2023-03-08 12:11:01 +0100

    Add git ignore

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "mtc-rg" {
   }
 }
 
-# Virtual Network
+# Azure Virtual Network
 resource "azurerm_virtual_network" "mtc-vn" {
   name                = "mtc-network"
   resource_group_name = azurerm_resource_group.mtc-rg.name
